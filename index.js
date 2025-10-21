cf9f93c6840a503b9a094eaf135b1ba900147c2c
Kolade Faith <faithkolade0507@gmail.com>
2025-10-21 15:34:15 +0100

    Refactor Product schema to enforce required fields and add timestamps; improve error logging in upload route

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use("/uploads", express.static("uploads"));
 const userRouters = require("./routes/user.route");
 app.use("/user", userRouters);
 const uploadRoutes = require("./routes/uploadRoutes")
-app.use("/api/admin", uploadRoutes);
-const productRoutes = require("./routes/productRoutes");
+// app.use("/api/admin", uploadRoutes);
+// const productRoutes = require("./routes/productRoutes");
 app.use("/api", productRoutes);
 // Public route
 app.get('/', (req, res) => {
